0b2f48b64da9d1e8739ee09ba4bd7f1890a8a211
Karl Brocklehurst <kairakau@gmail.com>
2019-02-01 10:02:15 +1300

    merge Ishan-katene to master

---
+++ client/components/Beat.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class Beat extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <div>
+        <p>
+          hi
+        </p>
+      </div>
+    )
+  }
+}
+
+export default Beat
